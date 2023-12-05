09eea95419c4b48e18bcb5725995386939b94892
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-12-05 05:19:12 +0200

    Test API

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -70,52 +72,56 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text("Drug Search"),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Image(
-                height: 200,
-                image: AssetImage('assets/logo.png'),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.horizontal(
-                    left: Radius.circular(30), // Adjust the value as needed
-                    right: Radius.circular(30), // Adjust the value as needed
-                  ),
-                  color: Colors.grey[200], // Adjust the background color as needed
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Image(
+                  height: 200,
+                  image: AssetImage('assets/logo.png'),
                 ),
-                child: Container(
-                  width: double.infinity, // Adjust the width as needed
-                  child: TextField(
-                    controller: _searchController,
-                    textAlign: TextAlign.center,
-                    textAlignVertical: TextAlignVertical.center,
-                    decoration: InputDecoration(
-                      hintText: "Search for drugs...",
-                      contentPadding: const EdgeInsets.symmetric(horizontal: 20),
-                      border: InputBorder.none, // Remove default border
-                      suffixIcon: IconButton(
-                        icon: const Icon(Icons.search),
-                        onPressed: () {
-                          // Handle search button press
-                          _handleSearch();
-                        },
-                      ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.horizontal(
+                      left: Radius.circular(30), // Adjust the value as needed
+                      right: Radius.circular(30), // Adjust the value as needed
                     ),
-                    onChanged: (value) {
-                      // Update suggestions dynamically based on user input
-                      _updateSuggestions(value);
-                    },
+                    color: Colors.grey[200], // Adjust the background color as needed
+                  ),
+                  child: SizedBox(
+                    width: double.infinity, // Adjust the width as needed
+                    child: TextField(
+                      controller: _searchController,
+                      textAlign: TextAlign.center,
+                      textAlignVertical: TextAlignVertical.center,
+                      style: const TextStyle(color: Colors.black),
+                      decoration: InputDecoration(
+                        hintText: "Search for drugs...",
+                        hintStyle: const TextStyle(color: Colors.black),
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 20),
+                        border: InputBorder.none, // Remove default border
+                        suffixIcon: IconButton(
+                          icon: const Icon(Icons.search, color: Colors.green),
+                          onPressed: () {
+                            // Handle search button press
+                            _handleSearch(_searchController.text);
+                          },
+                        ),
+                      ),
+                      onChanged: (value) {
+                        // Update suggestions dynamically based on user input
+                        _updateSuggestions(value);
+                      },
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 16),
-              _suggestedDrugs.isNotEmpty ? _buildSuggestions() : Container(),
-            ],
+                const SizedBox(height: 16),
+                _suggestedDrugs.isNotEmpty ? _buildSuggestions() : Container(),
+              ],
+            ),
           ),
         ),
       ),
@@ -123,27 +129,32 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Widget _buildSuggestions() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: _suggestedDrugs
-          .map((drug) => GestureDetector(
-        onTap: () {
-          // Handle suggestion tap
-          setState(() {
-            _selectedDrug = drug;
-            _searchController.text = drug;
-            _suggestedDrugs.clear();
-          });
-        },
-        child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 8),
-          child: Text(
-            drug,
-            style: const TextStyle(fontSize: 16),
-          ),
+    return SizedBox(
+      height: 200,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: _suggestedDrugs
+              .map((drug) => GestureDetector(
+            onTap: () {
+              // Handle suggestion tap
+              setState(() {
+                _selectedDrug = drug;
+                _searchController.text = drug;
+                _suggestedDrugs.clear();
+              });
+            },
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: Text(
+                drug,
+                style: const TextStyle(fontSize: 16),
+              ),
+            ),
+          ))
+              .toList(),
         ),
-      ))
-          .toList(),
+      ),
     );
   }
 
@@ -156,10 +167,49 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
-  void _handleSearch() {
-    // Implement logic to handle search
-    // For simplicity, just print the selected drug
+  void _handleSearch(String drug) async {
     print("Searching for $_selectedDrug...");
-    // Navigate to results screen or update UI with results
+    final url = Uri.parse('https://api.fda.gov/drug/event.json?search=$drug');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final drugInfo = jsonDecode(response.body);
+
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(drug),
+            content: Text(drugInfo['results'][0]['patient']['drug'][0]['medicinalproduct']),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(); // Close the dialog
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    } else {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Error'),
+            content: const Text('Failed to fetch drug information.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(); // Close the dialog
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
   }
 }
